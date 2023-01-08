d87112eee4b2c8dd1bf65c2bef836447f4385f6f
Jonas mendes <jonas.jsm2903@gmail.com>
2023-01-08 06:55:52 -0300

    transferindo

--- semáforo/script.js
+++ semáforo/script.js
@@ -1,5 +1,5 @@
-const img = document.getElementById('img')
-const buttons = document.getElementById('buttons')
+const img = document.querySelector('img')
+const buttons = document.querySelector('#buttons')
 let colorindex = 0;
 let setIntervalID = null
 
@@ -31,9 +31,9 @@ const stopautomatic = () => {
 }
 
 const tutnOn = {
-    'red':      () => img.src = "./semáforo/img/vermelho.png",
-    'yellow':   () => img.src = "./semáforo/img/amarelo.png",
-    'green':    () => img.src = "../semáforo/img/verde.png",
+    'red':      () => img.src = "/semáforo/img/vermelho.png",
+    'yellow':   () => img.src = "/semáforo/img/amarelo.png",
+    'green':    () => img.src = "/semáforo/img/verde.png",
     'automatic':() => setIntervalID = setInterval(changecolor,1000),
     
 }
