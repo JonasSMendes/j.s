d6ce7e128951057d0b951494af8ff3f7768f36a9
Jonas mendes <jonas.jsm2903@gmail.com>
2023-01-09 06:23:09 -0300

    css feito

--- slideshow/script.js
+++ slideshow/script.js
@@ -4,7 +4,7 @@ const images = [
     { 'id': '1' , 'url': "./img/chrono.jpg"},
     { 'id': '2' , 'url': "./img/inuyasha.jpg"},
     { 'id': '3' , 'url': './img/tenchi.jpg'},
-    { 'id': '4' , 'url': './img/tenjhotengo.jpg'},
+    { 'id': '4' , 'url': './img/tenjhotenge.jpg'},
     { 'id': '5' , 'url': './img/yuyuhakusho.jpg'},
     { 'id': '6' , 'url': './img/ippo.png' },
 ]
