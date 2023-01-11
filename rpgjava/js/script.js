6141b11234b5aafb14e323ca7180c0bb1224de04
Jonas mendes <jonas.jsm2903@gmail.com>
2023-01-11 11:33:46 -0300

    primeira tela
    primeira tela onde os personagens tem uma animação

---
+++ rpgjava/js/script.js
@@ -0,0 +1,13 @@
+function mago(){
+    window.location = 'reinos.html'
+    
+}
+function guerreiro(){
+    window.location = 'reinos.html'
+}
+function paladino(){
+    window.location = 'reinos.html'
+}
+function assassino(){
+    window.location = 'reinos.html'
+}
