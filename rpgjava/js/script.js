02986f989dcaafd442de33dfd2e21f562b83fae0
Jonas mendes <jonas.jsm2903@gmail.com>
2023-01-11 12:23:48 -0300

    tela dos reinos

--- rpgjava/js/script.js
+++ rpgjava/js/script.js
@@ -1,13 +1,26 @@
 function mago(){
     window.location = 'reinos.html'
-    
+
 }
+
 function guerreiro(){
     window.location = 'reinos.html'
 }
+
 function paladino(){
     window.location = 'reinos.html'
 }
+
 function assassino(){
     window.location = 'reinos.html'
+
+  
+}
+// pagina 2
+function gelo(){
+    window.location = 'text_gelo.html'
+}
+
+function deserto(){
+    window.location = 'text_deserto.html'
 }
