5a4891042b958ba57d7cb1a7db9f18153d5ebbb9
Jonas mendes <jonas.jsm2903@gmail.com>
2023-01-13 11:47:44 -0300

    criando o form

--- criador_de_formulario/js/script.js
+++ criador_de_formulario/js/script.js
@@ -1,3 +1,23 @@
+class Form{
+
+    items = [];
+
+    method = "GET"
+
+    constructor(container, method, action){
+        this.container = document.querySelector(container)
+        this.method = method
+        this.action = action
+
+    }
+    additem(item){
+        this.items.push(item)
+    }
+
+    
+}
+
+
 class input {
 
     _type = 'text'
@@ -21,13 +41,35 @@ class input {
 
 }
 
+class Button extends input{
+    constructor(label){
+        super('', label)
+        this.type = 'submit'
+    }
+}
+
 //implementação
+
+//formulario
+let form = new Form(".formArea", 'POST', 'https://site.com.br')
+
+
+//email
 let email = new input ('email', 'digite seu email')
 email.type = "email"
 email.reaquired = true
+form.additem(email);
 
 //senha
 
 let passworld = new input ('passworld', 'digite su senha')
 passworld.type = 'passworld'
+passworld.required = true
+form.additem(passworld)
 
+//botão
+
+let button = new Button('Enviar');
+form.additem(button);
+
+console.log(form.items)
