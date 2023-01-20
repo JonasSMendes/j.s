4316c85b140dd0c47784afeba7cba4a96233ffbc
Jonas mendes <jonas.jsm2903@gmail.com>
2023-01-20 10:59:37 -0300

    mudança para arrumar o width

--- menuResponsivo/js/script.js
+++ menuResponsivo/js/script.js
@@ -1,6 +1,14 @@
 
-function menu(elemento){
+function menu() {
     
-    elemento.style.display='none'
-    document.getElementById('menu-area').style.display = 'block'
+    let menuArea = document.getElementById('menu-area')
+
+    if(menuArea.style.width == '200px'){
+        menuArea.style.width = '0px'
+    }else{
+        menuArea.style.width = '200px'
+    }
+
+    
+     
 }
