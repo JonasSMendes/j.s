7f46a6488bf99eefea5d098194afb5ed1f7289e6
Jonas mendes <jonas.jsm2903@gmail.com>
2023-01-21 12:12:45 -0300

    switch

--- java.S/assets/JS/script.js
+++ java.S/assets/JS/script.js
@@ -1333,3 +1333,35 @@ carro.acelerar()
 
 */
 
+let dia = 7
+let diaNome =''
+
+switch(dia){
+    case 1:
+        diaNome = 'segunda-feira'
+        break
+    case 2:
+        diaNome = 'terça-feira'
+         break    
+    case 3:
+        diaNome = 'quarta-feira'
+        break     
+    case 4:
+        diaNome = 'quinta-feira'
+        break
+    case 5:
+        diaNome = 'sexta-feira'
+        break    
+    case 6:
+    case 7:
+        diaNome = 'final de semana'
+        break  
+    default:
+        diaNome = 'dia da semana'
+        break
+        
+        
+}
+document.getElementById('dia').innerHTML=`hoje é ${diaNome}`
+
+
