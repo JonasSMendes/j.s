135343fb58aa7382bee349a6682d895a9e95a785
Jonas mendes <jonas.jsm2903@gmail.com>
2023-01-25 12:06:54 -0300

    mudar de cor

--- java.S/assets/JS/script.js
+++ java.S/assets/JS/script.js
@@ -1413,3 +1413,29 @@ document.getElementsByTagName('button')
 
 document.querySelector('button').innerHTML="eu quero você";
 */
+
+function verde(){
+    document.querySelector('#exemplo').classList.remove('vermelho')
+    document.querySelector('#exemplo').classList.remove('azul')
+    document.querySelector('#exemplo').classList.remove('amarelo')
+    document.querySelector('#exemplo').classList.add('verde')
+
+}
+function vermelho(){
+    document.querySelector('#exemplo').classList.remove('vermelho')
+    document.querySelector('#exemplo').classList.remove('azul')
+    document.querySelector('#exemplo').classList.remove('amarelo')
+    document.querySelector('#exemplo').classList.add('vermelho')
+}
+function azul(){
+    document.querySelector('#exemplo').classList.remove('vermelho')
+    document.querySelector('#exemplo').classList.remove('azul')
+    document.querySelector('#exemplo').classList.remove('amarelo')
+    document.querySelector('#exemplo').classList.add('azul')
+}
+function amarelo(){
+    document.querySelector('#exemplo').classList.remove('vermelho')
+    document.querySelector('#exemplo').classList.remove('azul')
+    document.querySelector('#exemplo').classList.remove('amarelo')
+    document.querySelector('#exemplo').classList.add('amarelo')
+}
