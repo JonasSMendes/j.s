2f1d4bea2b5b369d8386ed773755a2362f962b42
Jonas mendes <jonas.jsm2903@gmail.com>
2023-01-27 07:50:35 -0300

    mudanças cores

--- dashBord/js/script.js
+++ dashBord/js/script.js
@@ -1,9 +1,12 @@
+
+
+
 const ctx = document.getElementById('myChart');
 
 new Chart(ctx, {
   type: 'bar',
   data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    labels: ['janeiro', 'fevereiro', 'Yellow', 'Green', 'Purple', 'Orange'],
     datasets: [{
       label: '# of Votes',
       data: [12, 19, 3, 5, 2, 3],
@@ -18,3 +21,4 @@ new Chart(ctx, {
     }
   }
 });
+
