144a90d44466fdd5a30460cae310315b8f3975ed
Jonas mendes <jonas.jsm2903@gmail.com>
2023-01-28 12:24:47 -0300

    calculo de soma

---
+++ calculadora/js/script.js
@@ -0,0 +1,62 @@
+
+
+const igual = document.getElementById('igual')
+
+function somar(){
+    const x = document.getElementById('x').value
+    const y = document.getElementById('y').value
+
+    if( x !== "" && y !== ""){
+
+        const somar = parseInt(x) + parseInt(y)
+        igual.textContent = somar
+    }else{
+        alert('preencha todos os campos')
+    }
+    
+   
+}
+function subtrair(){
+    const x = document.getElementById('x').value
+    const y = document.getElementById('y').value
+
+    if( x !== "" && y !== ""){
+
+        const somar = parseInt(x) + parseInt(y)
+        igual.textContent = somar
+    }else{
+        alert('preencha todos os campos')
+    }
+    
+   
+}
+function dividir(){
+    const x = document.getElementById('x').value
+    const y = document.getElementById('y').value
+
+    if( x !== "" && y !== ""){
+
+        const somar = parseInt(x) + parseInt(y)
+        igual.textContent = somar
+    }else{
+        alert('preencha todos os campos')
+    }
+    
+   
+}
+function multiplicar(){
+    const x = document.getElementById('x').value
+    const y = document.getElementById('y').value
+
+    if( x !== "" && y !== ""){
+
+        const somar = parseInt(x) + parseInt(y)
+        igual.textContent = somar
+    }else{
+        alert('preencha todos os campos')
+    }
+    
+   
+}
+
+
