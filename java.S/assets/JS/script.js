6ecbd0bc006378ed303d3d7d0fec1c7fe98eec67
Jonas mendes <jonas.jsm2903@gmail.com>
2023-02-06 12:52:04 -0300

    json

--- java.S/assets/JS/script.js
+++ java.S/assets/JS/script.js
@@ -1967,4 +1967,36 @@ temp.catch(function (error){
 })
 
 */
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
+//fetch 1
+
+
+function loadPost(){
+    document.getElementById('post').innerHTML = 'carregando...'
+
+    fetch('https://jsonplaceholder.typicode.com/posts',)
+    .then(function(resultado){
+        return resultado.json();
+    })
+    .then(function(json){
+        montarBlog(json)
+
+    })
+    .catch(function(){
+        console.log('deu ruim meu chapa')
+    })
+}
+
+function montarBlog(lista){
+    let html = ''
+
+    for(let i in lista){
+        html += '<h3>'+lista[i].title+'</h3>'
+        html += lista[i].body+'<br/>'
+        html += '<hr/>';
+    }
+
+    document.getElementById('post').innerHTML = html
+}
+//
