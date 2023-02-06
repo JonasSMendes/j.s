e4e4d1890c5e323c85c190f3f3f6f95b8579caed
Jonas mendes <jonas.jsm2903@gmail.com>
2023-02-06 18:45:37 -0300

    botão

---
+++ valorantIndex/js/script.js
@@ -0,0 +1,53 @@
+
+function info(){
+    
+    document.querySelector('.INFO').style.display = 'block'
+
+    
+    document.querySelector('.C').style.display = 'none'
+    document.querySelector('.Q').style.display = 'none'
+    document.querySelector('.E').style.display = 'none'
+    document.querySelector('.X').style.display = 'none'
+
+}
+function c(){
+
+    document.querySelector('.C').style.display = 'block'
+    
+    document.querySelector('.INFO').style.display = 'none'
+    document.querySelector('.Q').style.display = 'none'
+    document.querySelector('.E').style.display = 'none'
+    document.querySelector('.X').style.display = 'none'
+
+}
+function q(){
+
+    document.querySelector('.Q').style.display = 'block'
+
+    
+    document.querySelector('.INFO').style.display = 'none'
+    document.querySelector('.C').style.display = 'none'
+    document.querySelector('.E').style.display = 'none'
+    document.querySelector('.X').style.display = 'none'
+
+}
+function e(){
+
+    document.querySelector('.E').style.display = 'block'
+    
+    document.querySelector('.INFO').style.display ='none'
+    document.querySelector('.C').style.display = 'none'
+    document.querySelector('.Q').style.display = 'none'
+    document.querySelector('.X').style.display = 'none'
+
+}
+function x(){
+    document.querySelector('.X').style.display = 'block'
+    
+    document.querySelector('.INFO').style.display ='none'
+    document.querySelector('.C').style.display = 'none'
+    document.querySelector('.Q').style.display = 'none'
+    document.querySelector('.E').style.display = 'none'
+    
+
+}
