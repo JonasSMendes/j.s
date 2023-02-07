a254a0f9c1d5694faf698a7f5b21b54469289d94
Jonas mendes <jonas.jsm2903@gmail.com>
2023-02-07 12:37:40 -0300

    adicionando as imagens e os preços

--- pizzariajava/script.js
+++ pizzariajava/script.js
@@ -4,7 +4,14 @@ const cs = (el) => document.querySelectorAll(el)
 
 pizzaJson.map((item, index)=>{
     let pizzaItem = c('.models .pizza-item').cloneNode(true)
-//preencher as informações com pizza-item
+    //preencher as informações com pizza-item
+
+    pizzaItem.querySelector('.pizza-item--img img').src= item.img
+
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`
+   
 
     c('.pizza-area').append( pizzaItem );
 
