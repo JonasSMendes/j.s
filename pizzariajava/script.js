fe8c408e7c77e104808f06c208d5881332713188
Jonas mendes <jonas.jsm2903@gmail.com>
2023-02-07 12:55:33 -0300

    adicionando a div de compras

--- pizzariajava/script.js
+++ pizzariajava/script.js
@@ -7,12 +7,21 @@ pizzaJson.map((item, index)=>{
     //preencher as informações com pizza-item
 
     pizzaItem.querySelector('.pizza-item--img img').src= item.img
-
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`
    
+    pizzaItem.querySelector('a').addEventListener('click', (e)=>{
+        e.preventDefault();
+
+
+        c('.pizzaWindowArea').style.opacity = 0
+      c('.pizzaWindowArea').style.display = 'flex';
+      setTimeout(()=>{
+        c('.pizzaWindowArea').style.opacity = 1
+      }, 200)
+    })
 
     c('.pizza-area').append( pizzaItem );
 
-})
+});
