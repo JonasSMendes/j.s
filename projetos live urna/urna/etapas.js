a19db69ef10d756cdb19d3f4fad2e35a4e47eca6
Jonas mendes <jonas.jsm2903@gmail.com>
2023-02-13 09:30:14 -0300

    criando metedo de etapa

---
+++ projetos live urna/urna/etapas.js
@@ -0,0 +1,57 @@
+let etapas = [
+    {
+    titulo: 'veriador',
+    numero: 5,
+    candidatos:[
+        {
+            numero: 38111,
+            nome: 'fulano de tal',
+            pertido: 'ABC',
+            fotos: [
+                {url: './images/38111.jpg', legenda: 'vereador', small: false}
+            ]
+        },
+        {
+            numero: 77222,
+            name: 'beltrano da silva',
+            partido: 'DEFG',
+            fotos:[
+                {
+                    url: './images/77222.jpg', legenda: 'prefeito'
+                }
+            ]
+            
+        },
+    ]
+    
+}, // fim do primeiro obejeto
+    {  
+        titulo: 'Prefeito',
+    numero: 2,
+    candidatos:[
+        {
+            numero: 84,
+            nome: 'coitado da tia',
+            vice: 'cic',
+            pertido: 'ABC',
+            fotos: [
+                {url: './images/99.jpg', legenda: 'prefeito'},
+                {url: './images/99_2.jpg', legenda: 'vice-prefeito', small: false}
+            ]
+        },
+        {
+            numero: 99,
+            name: 'zulano',
+            vice: 'zul' ,
+            partido: 'QWERT',
+            fotos:[
+                { url: './images/84.jpg', legenda: 'prefeito' },
+                {url: './images/84_2.jpg', legenda: 'vice-prefeito', small:false}
+            ]
+            
+        },
+    ]
+    
+    }
+
+]
