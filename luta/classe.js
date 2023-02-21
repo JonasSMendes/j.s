f9e028d78c9c5a7ec31f87559e7b8a5d346ef602
Jonas mendes <jonas.jsm2903@gmail.com>
2023-02-21 08:57:47 -0300

    att
    implementação de ataque e defesa

--- luta/classe.js
+++ luta/classe.js
@@ -34,8 +34,8 @@ class knigth extends caracter {
 class sorcerer extends caracter{
     constructor(name){
         super(name);
-        this.life = 80;
-        this.attack = 15;
+        this.life = 60;
+        this.attack = 20;
         this.defense = 3;
         this.maxLife = this.life;
     }
@@ -52,9 +52,9 @@ class litlemonster extends caracter{
 class bigMonster extends caracter{
     constructor(){
         super('bigMonster');
-        this.life = 180;
-        this.attack = 6;
-        this.defense = 2;
+        this.life = 200;
+        this.attack = 8;
+        this.defense = 8;
         this.maxLife = this.life;
     }
 }
@@ -68,15 +68,45 @@ class Stage{
     start(){
         this.update()
         // evento do botão de atacar
+
+        this.figther1El.querySelector('.atackButton').addEventListener('click', ()=> this.doAttack(this.figther1,this.figther2))
+        this.figther2El.querySelector('.atackButton').addEventListener('click', ()=> this.doAttack(this.figther2,this.figther1))
     }
     update(){
         //figther 1
 
-        this.figther1El.querySelector('.name').innerHTML= this.figther1.name;
-        
+        this.figther1El.querySelector('.name').innerHTML= `${this.figther1.name} - ${this.figther1.life.toFixed(1)}`;
+        let f1Pct = (this.figther1.life / this.figther1.maxLife) * 100
+        this.figther1El.querySelector('.bar').style.width = `${f1Pct}%`
         //figther2
 
-        this.figther2El.querySelector('.name').innerHTML= this.figther2.name;
+        this.figther2El.querySelector('.name').innerHTML= `${this.figther2.name} - ${this.figther2.life.toFixed(1)}`;
+        let f2Pct = (this.figther2.life / this.figther2.maxLife) * 100
+        this.figther2El.querySelector('.bar').style.width = `${f2Pct}%`
+    }
 
+    doAttack(attacking, attacked){
+       // console.log(`${attacking.name} esta atacando ${attacked.name}`)
+        if(attacking.life <= 0 || attacked.life <= 0){
+            console.log('atacando cadaver')
+            return
+        }
+
+        let attackFactor = (Math.random() * 2).toFixed(2)
+        let defenseFactor = (Math.random() * 2).toFixed(2)
+
+        let actualAttack = attacking.attack * attackFactor;
+        let actualDefense = attacked.defense * defenseFactor
+        console.log(actualAttack , actualDefense)
+
+        if(actualAttack > actualDefense){
+            attacked.life -= actualAttack
+            console.log(`${attacking.name} causou ${actualAttack.toFixed(2)} em ${attacked.name}`)
+        }else{
+            console.log(`${attacked.name} conseguiu defender`)
+        }
+
+
+        this.update()
     }
 }
