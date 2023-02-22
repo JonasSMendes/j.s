5ae9fb5903c0e763a18bc7fb477c2717de67872a
Jonas mendes <jonas.jsm2903@gmail.com>
2023-02-22 12:19:03 -0300

    att
    adicionando a funcionalidade do mouse

--- quadroDeDesenho/script.js
+++ quadroDeDesenho/script.js
@@ -1,6 +1,9 @@
 //inicio
 
 let currenColor = 'black'
+let canDraw = false
+let pointX = 0;
+let pointY = 0;
 
 let screem = document.querySelector('#tela') 
 let ctx = screem.getContext('2d')
@@ -9,11 +12,38 @@ document.querySelectorAll('.colorArea .color').forEach(item => {
     item.addEventListener('click', colorClickEvent);
 
 });
+screem.addEventListener('mousedown',mouseDownEvent)
+screem.addEventListener('mousemove',mousemoveEvent)
+screem.addEventListener('mouseup', mouseupEvent)
+/* passo a passo para desenhar no canvas
+
+- quando eu clicar e ABaixar o modo desenho será ativado
+- quando o mouse se mover enquanto estiver ativado, desenhe
+- quando clique do mouse levantar para o modo desenho
+
+*/
 //funçoes
+
 function colorClickEvent(e){
     let color = e.target.getAttribute('data-color')
     currenColor = color
 
     document.querySelector('.color.active').classList.remove('active')
     e.target.classList.add('active')
+}
+
+function mouseDownEvent(){
+    canDraw = true
+
+    pointX = e.pageX - screem.offsetLeft
+    pointY = e.pageY - screem.offsetTop
+    
+}
+function mousemoveEvent(e){
+    if(canDraw){
+        
+    }
+}
+function mouseupEvent(){
+   canDraw = false
 }
