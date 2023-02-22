115f5e11f8803f0925079f8de1f5d7af36a788ef
Jonas mendes <jonas.jsm2903@gmail.com>
2023-02-22 19:24:33 -0300

    finalizado
    quando de desenho finalizado

--- quadroDeDesenho/script.js
+++ quadroDeDesenho/script.js
@@ -2,8 +2,8 @@
 
 let currenColor = 'black'
 let canDraw = false
-let pointX = 0;
-let pointY = 0;
+let mouseX = 0;
+let mouseY = 0;
 
 let screem = document.querySelector('#tela') 
 let ctx = screem.getContext('2d')
@@ -15,6 +15,7 @@ document.querySelectorAll('.colorArea .color').forEach(item => {
 screem.addEventListener('mousedown',mouseDownEvent)
 screem.addEventListener('mousemove',mousemoveEvent)
 screem.addEventListener('mouseup', mouseupEvent)
+document.querySelector('.clear').addEventListener('click', clearScreem)
 /* passo a passo para desenhar no canvas
 
 - quando eu clicar e ABaixar o modo desenho será ativado
@@ -32,18 +33,42 @@ function colorClickEvent(e){
     e.target.classList.add('active')
 }
 
-function mouseDownEvent(){
+function mouseDownEvent(e){
     canDraw = true
 
-    pointX = e.pageX - screem.offsetLeft
-    pointY = e.pageY - screem.offsetTop
+    mouseX = e.pageX - screem.offsetLeft
+    mouseY = e.pageY - screem.offsetTop
     
 }
 function mousemoveEvent(e){
     if(canDraw){
-        
+        Draw(e.pageX, e.pageY)
     }
 }
 function mouseupEvent(){
    canDraw = false
+}
+
+function Draw(x, y){
+    let pointX = x - screem.offsetLeft
+    let pointY = y - screem.offsetTop
+
+    ctx.beginPath();
+    ctx.lineWidth = 5;
+    ctx.lineJoin = 'round';
+    ctx.moveTo(mouseX,mouseY);
+    ctx.lineTo(pointX,pointY);
+    ctx.closePath();
+    ctx.strokeStyle = currenColor;
+    ctx.stroke();
+
+
+    mouseX = pointX
+    mouseY = pointY
+}
+
+function clearScreem(){
+    ctx.setTransform(1,0,0,1,0,0)
+    ctx.clearRect(0 , 0, ctx.canvas.width, ctx.canvas.height)
+
 }
